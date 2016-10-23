e726cd9a7eb51627f729aabb39cbdea73dd412b9
Misty Jenkins <misty.wallace@aggiemail.usu.edu>
2016-10-23 17:23:48 -0600

    Done, but not tested. Scary, I know.

--- analyze_lums_and_temps.py
+++ analyze_lums_and_temps.py
@@ -1,8 +1,4 @@
 import argparse
-import cv2
-import sys
-import os
-import fnmatch
 import re
 
 ap = argparse.ArgumentParser()
@@ -11,25 +7,41 @@ ap.add_argument('-tf', '--temp_file', required = True, help = 'Path to temp file
 args = vars(ap.parse_args())
 
 ## define regexes
-lum_entry_pat  = r''
-temp_entry_pat = r''
+# lum entry example:  2016-09-21_17-29-43.png 96.1040781279
+# group 1: hour
+# group 2: luminosity
+lum_entry_pat  = r'\d+-\d+-\d+_(\d+)-\d+-\d+\.png\s+(\d+\.\d+)'
+# temp entry example: 2016-09-21_17-29-44 31.0
+temp_entry_pat = r'\d+-\d+-\d+_(\d+)-\d+-\d+\s+(\d+\.\d+)'
 
 ## define two dictionaries
 lum_tbl = {}
 tmp_tbl = {}
 
 with open(args['lum_file']) as infile:
+    global lum_tbl
     for line in infile:
         m = re.match(lum_entry_pat, line)
         if m != None:
-            ## your code to fill up lum_tbl
+            hour = m.group(1)
+            lum = m.group(2)
+            if hour in lum_tbl:
+                lum_tbl[hour].append(lum)
+            else:
+                lum_tbl[hour] = [lum]
 
 with open(args['temp_file']) as infile:
+    global tmp_tbl
     for line in infile:
         print line
         m = re.match(temp_entry_pat, line)
         if m != None:
-            ## your code to fill up tmp_tbl
+            hour = m.group(1)
+            tmp = m.group(2)
+            if hour in lum_tbl:
+                tmp_tbl[hour].append(tmp)
+            else:
+                tmp_tbl[hour] = [tmp]
 
 ## print tables and averages
 print('Luminosity Table')
@@ -51,11 +63,3 @@ print('Temperature Averages')
 for h, temps in tmp_tbl.items():
     print h, '-->', sum(temps)/len(temps)
 print
-
-
-
-       
-
-
-    
-
--- gen_avrg_lum.py
+++ gen_avrg_lum.py
@@ -1,9 +1,8 @@
 import argparse
 import cv2
-import sys
-import os
 import fnmatch
-import re
+import os
+import sys
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-id', '--idir', required = True, help = 'Path to image')
@@ -17,22 +16,41 @@ def display_image_pix(image, h, w):
             print list(image_pix[r][c]), ' ',
         print
 
+
 ## luminosity conversion
 def luminosity(rgb, rcoeff=0.2126, gcoeff=0.7152, bcoeff=0.0722):
     return rcoeff*rgb[0]+gcoeff*rgb[1]+bcoeff*rgb[2]
 
-def compute_avrg_luminosity(imagepath):
-    pass
 
-def gen_avrg_lumin_for_dir(imgdir, filepat):
-    pass
+## This function takes a path to an image and computes the average luminosity of the image by computing
+## the sum of the pixel luminosities and dividing it by the number of pixels.
+def compute_avrg_luminosity(imagepath):
+    image = cv2.imread(imagepath)
+    (rows, cols, num_channels) = image.shape
+    totalLum = 0;
+    for r in xrange(rows):
+        for c in xrange(cols):
+            totalLum += luminosity(image[r,c])
+
+    return totalLum/image.size
+
+
+# This generator takes an directory where images are stored and a regular
+# expression that specifies which images should be processed and yields 2-tuples
+# of image filenames that match the pattern and their average luminosities.
+def gen_avrg_lumin_for_dir(imgdir, filepat=r'*.png'):
+    fns = generate_file_names(filepat, imgdir)
+    for f in fns:
+        yield (f, compute_avrg_luminosity(f))
+
+
+def generate_file_names(fnpat, rootdir):
+  for path, dirlist, filelist in os.walk(rootdir):
+    for file_name in fnmatch.filter(filelist, fnpat):
+        yield os.path.join(path, file_name)
+
 
 ## run ghe generator and output into STDOUT           
 for fp, lum_avrg in gen_avrg_lumin_for_dir(args['idir'], r'*.png'):
     sys.stdout.write(fp + '\t'  + str(lum_avrg) + '\n')
     sys.stdout.flush()
-       
-
-
-    
-
